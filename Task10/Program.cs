57505e88f64ec7e2c0f60ad4307d3f6066403ba0
Mariya Nemzorova <nemzorova777@mail.ru>
2022-12-26 00:34:13 +0700

    Solve Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10
+//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int second = number % 100;
+
+int result = second / 10;
+Console.WriteLine($"Вторая цифра введённого числа: {result}");
