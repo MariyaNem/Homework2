e0c748c8f7e0b7e7a97cf2336ac4244f36cc622a
Mariya Nemzorova <nemzorova777@mail.ru>
2022-12-26 17:17:02 +0700

    Fix mistakes Task10

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -8,7 +8,10 @@
 Console.Write("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-int second = number % 100;
-
-int result = second / 10;
-Console.WriteLine($"Вторая цифра введённого числа: {result}");
+if (number < 1000 && number > 99)
+{
+    int second = number % 100;
+    int result = second / 10;
+    Console.WriteLine($"Вторая цифра введённого числа: {result}");
+}
+else Console.Write("Вы ввели не трёхзначное число");
